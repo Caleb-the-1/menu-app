fac0665174b24eeeb790abc6f4307d2386dad72c
caleb-the-1 <caleb.orabe@500chow.com>
2025-08-07 11:32:21 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import MenuItem from './MenuItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '30px' }}>
+      <h1 style={{ textAlign: 'center', fontSize: '40px' }}>MENU</h1>
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap:'20px'}}>
+        <MenuItem
+          title="Ekuru"
+          description="kuru beans"
+          price="500"
+          image="/Ekuru.jpg"
+        />
+        <MenuItem
+          title="Jollof Rice x"
+          description="Smokey Jay rice"
+          price="1000"
+          image="/Jellof Rice X.jpg"
+        />
+        <MenuItem
+          title="Fried Rice x"
+          description="Fried Rice"
+          price="800"
+          image="/Fried Rice x.jpg"
+        />
+        <MenuItem
+          title="Chairman Chow e"
+          description="A combo of our signature rice or pasta..."
+          price="3650"
+          image="https://via.placeholder.com/300x150?text=Chairman+Chow"
+        />
+        <MenuItem
+          title="Chairman"
+          description="Chow for Chairman"
+          price="2500"
+          image="https://via.placeholder.com/300x150?text=Chairman"
+        />
+        <MenuItem
+          title="Yam"
+          description="New stuff"
+          price="2000"
+          image="/Yam.jpg"
+        />
+        <MenuItem
+          title="Plantain"
+          description="Plantainnnnnnnnnnnnnnnn!!!"
+          price="400"
+          image="/Plantain.jpg"
+        />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
